0b128f6a3d0c89f6070fd5dabcfa0b435d887fc3
iclipsvideo <iclipsvideo@gmail.com>
2023-07-05 07:49:22 +0200

    treeview examples

---
+++ src/projects/json-tree/src/components/VueTwoExample/VueTwoExample.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TreeItem from "./components/TreeItem.vue";
+
+const treeData = ref({
+  name: "My Tree",
+  children: [
+    { name: "hello" },
+    { name: "wat" },
+    {
+      name: "child folder",
+      children: [
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }],
+        },
+        { name: "hello" },
+        { name: "wat" },
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }],
+        },
+      ],
+    },
+  ],
+});
+</script>
+
+<template>
+  <ul id="demo">
+    <TreeItem
+      class="item"
+      :item="treeData"
+      ...make-folder="makeFolder"
+      ...add-item="addItem"
+    ></TreeItem>
+  </ul>
+</template>
