3b20bdabb3bd632387c2f00c6a130f0db687c5f9
Lyall <lyall.vanderlinde@iriesoft.net>
2023-07-26 18:00:04 +0200

    put boise on the map

--- src/projects/json-tree/src/App.vue
+++ src/projects/json-tree/src/App.vue
@@ -4,18 +4,35 @@
 import VueTwoExample from "./components/VueTwoExample/VueTwoExample.vue";
 import SimpleSlot from "./components/SimpleSlot/SimpleSlot.vue";
 import Sample from "./components/ListTree/Sample.vue";
+import { ref } from "vue";
+
+
+const opened = ref(false)
 </script>
 
 <template>
+
   <div>
+    <button @click="opened = !opened">Toggle</button>
+
     <div
-      class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform ---translate-x-full --sm:translate-x-0"
+    :class="[{'-translate-x-full': opened }]"
+      class="fixed top-20 left-0 z-40 w-64 h-screen transition-transform --sm:translate-x-0"
     >
       <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <Sample />
       </div>
     </div>
+
+
+  <div>
+  <p>
+    <h2>todo</h2>
+  </p>    
   </div>
+
+  </div>
+
 </template>
 
 <style></style>
